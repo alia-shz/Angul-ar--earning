e8fe7395c0532a3c12ef93dd0800ab6f108a0cbe
alia-shz <aliashiraz05@gmail.com>
2023-09-08 08:50:31 +0530

    Second Commit

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -9,11 +9,51 @@ import { Component } from '@angular/core';
 
 
 export class FirstComponentComponent {
-
   fname: string = '';
   lname: string = '';
   email: string = '';
   gender: string = '';
-  language: string = '';
+  final_user:user_det = new user_det();
+  langlist:language_s[]=[];
+  // final_langlist:string='';
+  ngOnInit()
+  {
+    this.get_lang()
+  }
 
+  get_lang()
+  {
+    this.langlist=[
+      {langname:"Malayalam",isselected:false},
+      {langname:"English",isselected:false},
+      {langname:"Hindi",isselected:false}
+    ]
+  }
+
+  onsubmit()
+  {
+      
+      this.final_user.ufname =  this.fname;
+      this.final_user.ulname = this.lname;
+      this.final_user.uemail = this.email;
+      this.final_user.ugender = this.gender;
+      this.final_user.lang_n = this.langlist.filter(x=>x.isselected==true).map(x=>x.langname).join(",").toString();
+      console.log(this.final_user)
+  }
+  
+  
 }
+
+class language_s{
+  langname: string = '';
+  isselected: boolean = false;
+}
+
+class user_det{
+  ufname: string = '';
+  ulname: string = '';
+  uemail: string = '';
+  ugender: string = '';
+  lang_n:string = '';
+}
+
